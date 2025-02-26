c98cb5b6f621456b92a8301659a0f31adc0b131c
miray beyza <120053146+miray-b@users.noreply.github.com>
2025-02-26 03:41:38 +0300

    Add files via upload

---
+++ asal.py
@@ -0,0 +1,15 @@
+control_sayi = int(input("Asallığını kontrol etmek istediğiniz sayı: "))
+
+def asal_control(control_sayi):
+    if control_sayi < 2:
+        return False
+    for i in range(2, int(control_sayi ** 0.5) + 1):
+        if control_sayi % i == 0:
+            return False
+    return True
+
+if asal_control(control_sayi):
+    print("Asal bir sayıdır.")
+
+else:
+    print("Asal bir sayı değildir.")
